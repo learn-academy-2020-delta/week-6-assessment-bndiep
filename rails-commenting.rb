7dafc60d7afc8c1b61ffa9cfe59bcb3b3d2cba8a
bnddiep <bach.nhu.diep@gmail.com>
2020-10-17 21:01:45 -0700

    Complete assessment

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# 1)
+# 1) The name of the BlogPosts controller that will hold all the methods necessary to route an external request
 class BlogPostsController < ApplicationController
   def index
-    # 2)
+    # 2) The instance variable posts is assigned to all the BlogPosts, so this allows the user to get the entire hash of all blog posts that have been made.
     @posts = BlogPost.all
   end
 
   def show
-    # 3)
+    # 3) The instance variable posts is assigned to a single blog post, which can be found by providing the specific id number associated with that blog post as a parameter in the blogs url.
     @post = BlogPost.find(params[:id])
   end
 
-  # 4)
+  # 4) Method associated with the post HTTP verb that will assist in creating a new blog post.
   def new
     @post = Post.new
   end
 
   def create
-    # 5)
+    # 5) Instance variable post is assigned to a new BlogPost that takes a parameter, which is the blogs data fields, title and content. The title and content is required because it appears in the protected, private section. 
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -39,7 +39,7 @@ class BlogPostsController < ApplicationController
 
   def update
     @post = BlogPost.find(params[:id])
-    # 6)
+    # 6) Updates the specific blog post by requiring the user to edit the title and the content of the blog post for it to be valid.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -53,15 +53,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # 7)
+      # 7) If the blog post cannot be destroyed, redirect the user to the blog post they were trying to destroy (because the method was unsuccessful).
       redirect_to blog_post_path(@post)
     end
   end
 
-  # 8)
+  # 8) The strong params section is separated by the private keyword.
   private
   def blog_post_params
-    # 9)
+    # 9) The fields that a user can add or is required for the blog post database is the title and the content.
     params.require(:blog_post).permit(:title, :content)
   end
 
@@ -71,6 +71,6 @@ end
 # FILE: app/models/blog_post.rb
 
 class BlogPost < ApplicationRecord
-  # 10)
+  # 10) The BlogPost model has many comments. The Comments model belong to BlogPost. Specifies the relationship between a BlogPost and Comments.
   has_many :comments
 end
